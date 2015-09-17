441af2bbf1b84275ebde9347239516b77c07f058
shaheermian <shaheermian@gmail.com>
2015-09-17 20:10:26 +0500

    Finalized according to final.jpg
    The page has been finalized according to the "final.jpg" image included
    in the "new-stuff" folder in the outbound branch

--- src/Avenue/CheckoutBundle/Controller/DefaultController - original.php
+++ src/Avenue/CheckoutBundle/Controller/DefaultController - original.php
@@ -17,7 +17,7 @@ class DefaultController extends Controller
         $entity = new \stdClass();
         $entity->id = 1;
 
-        $productTrial = new \stdClass();
+        /*$productTrial = new \stdClass();
         $productTrial->id = 1;
 
         $priceTrial = new \stdClass();
@@ -33,7 +33,7 @@ class DefaultController extends Controller
         $productPopular->id = 3;
 
         $pricePopular = new \stdClass();
-        $pricePopular->price = 100;
+        $pricePopular->price = 100;*/
 
         $productMaximum = new \stdClass();
         $productMaximum->id = 4;
@@ -55,12 +55,12 @@ class DefaultController extends Controller
         return array(
             'entity' => $entity,
             'form' => $form->createView(),
-            'productTrial' => $productTrial,
+            /*'productTrial' => $productTrial,
             'priceTrial' => $priceTrial,
             'productStarter' => $productStarter,
             'priceStarter' => $priceStarter,
             'productPopular' => $productPopular,
-            'pricePopular' => $pricePopular,
+            'pricePopular' => $pricePopular,*/
             'productMaximum' => $productMaximum,
             'priceMaximum' => $priceMaximum,
             'currentItem' => $currentItem,
