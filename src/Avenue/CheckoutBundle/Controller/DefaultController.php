65ed54594f211e6e191be2874416689a1f88db9e
shaheermian <shaheermian@gmail.com>
2015-10-03 21:14:53 +0500

    Avenue Hair
    - Make form validation as of master branch - (done)
    - Make modal as of master branch - (done)
    - Make alert box as of master branch - (alert box was buggy and not
    being controlled properly. Instead a generic error box has been incldued
    under the form that will list the form errors)
    - Add order2.html to src/Avenue/CheckoutBundle/Resources/views/base.html
    - (done)
    - Make product name, prices, before prices work as objects as of master
    branch - (done)
    - Make shipping work as of master branch (products without shipping, and
    box in the bottom) - (done)

--- src/Avenue/CheckoutBundle/Controller/DefaultController.php
+++ src/Avenue/CheckoutBundle/Controller/DefaultController.php
@@ -17,7 +17,7 @@ class DefaultController extends Controller
         $entity = new \stdClass();
         $entity->id = 1;
 
-        $productTrial = new \stdClass();
+        /*$productTrial = new \stdClass();
         $productTrial->id = 1;
 
         $priceTrial = new \stdClass();
@@ -39,11 +39,78 @@ class DefaultController extends Controller
         $productMaximum->id = 4;
 
         $priceMaximum = new \stdClass();
-        $priceMaximum->price = 349.95;
+        $priceMaximum->price = 349.95;*/
+		
+		$productSingle = new \stdClass();
+		$productSingle->id = 1;
+		$productSingle->name = 'SINGLE PRODUCT';
+		$productSingle->listPrice = 384;
+		$productSingle->listPricePre = (strpos($productSingle->listPrice,'.') > -1) ? ''.$this->pricePreDec($productSingle->listPrice).'' : ''.$productSingle->listPrice.'';
+		$productSingle->listPricePost = (strpos($productSingle->listPrice,'.') > -1) ? ''.$this->pricePostDec($productSingle->listPrice).'' : ''; //currently set to show nothing in the <sup> tag. You can change it to '00' if you want.
+		$productSingle->price = 130.95;
+		$productSingle->pricePre = (strpos($productSingle->price,'.') > -1) ? ''.$this->pricePreDec($productSingle->price).'' : ''.$productSingle->price.'';
+		$productSingle->pricePost = (strpos($productSingle->price,'.') > -1) ? ''.$this->pricePostDec($productSingle->price).'' : ''; //currently set to show nothing in the <sup> tag. You can change it to '00' if you want.
+		$productSingle->shippingPrice = 45;
+		$productSingle->shippingText = 'Priority Shipping';
+		$productSingle->total = $productSingle->price + $productSingle->shippingPrice;
+		
+		$productBundleOffer = new \stdClass();
+		$productBundleOffer->id = 2;
+		$productBundleOffer->name = 'BUY 2 GET 1 FREE';
+		$productBundleOffer->listPrice = 384;
+		$productBundleOffer->listPricePre = (strpos($productBundleOffer->listPrice,'.') > -1) ? ''.$this->pricePreDec($productBundleOffer->listPrice).'' : ''.$productBundleOffer->listPrice.'';
+		$productBundleOffer->listPricePost = (strpos($productBundleOffer->listPrice,'.') > -1) ? ''.$this->pricePostDec($productBundleOffer->listPrice).'' : ''; //currently set to show nothing in the <sup> tag. You can change it to '00' if you want.
+		$productBundleOffer->price = 130.95;
+		$productBundleOffer->pricePre = (strpos($productBundleOffer->price,'.') > -1) ? ''.$this->pricePreDec($productBundleOffer->price).'' : ''.$productBundleOffer->price.'';
+		$productBundleOffer->pricePost = (strpos($productBundleOffer->price,'.') > -1) ? ''.$this->pricePostDec($productBundleOffer->price).'' : ''; //currently set to show nothing in the <sup> tag. You can change it to '00' if you want.
+		$productBundleOffer->shippingPrice = 45;
+		$productBundleOffer->shippingText = 'Priority Shipping';
+		$productBundleOffer->total = $productBundleOffer->price + $productBundleOffer->shippingPrice;
+
+		$productStarter = new \stdClass();
+		$productStarter->id = 3;
+		$productStarter->name = 'STARTER';
+		$productStarter->listPrice = 157.90;
+		$productStarter->listPricePre = (strpos($productStarter->listPrice,'.') > -1) ? ''.$this->pricePreDec($productStarter->listPrice).'' : ''.$productStarter->listPrice.'';
+		$productStarter->listPricePost = (strpos($productStarter->listPrice,'.') > -1) ? ''.$this->pricePostDec($productStarter->listPrice).'' : ''; //currently set to show nothing in the <sup> tag. You can change it to '00' if you want.
+		$productStarter->price = 78.95;
+		$productStarter->pricePre = (strpos($productStarter->price,'.') > -1) ? ''.$this->pricePreDec($productStarter->price).'' : ''.$productStarter->price.'';
+		$productStarter->pricePost = (strpos($productStarter->price,'.') > -1) ? ''.$this->pricePostDec($productStarter->price).'' : ''; //currently set to show nothing in the <sup> tag. You can change it to '00' if you want.
+		$productStarter->shippingPrice = 0;
+		$productStarter->shippingText = 'Free Priority Shipping';
+		$productStarter->total = $productStarter->price + $productStarter->shippingPrice;
+		
+		$productLarge = new \stdClass();
+		$productLarge->id = 4;
+		$productLarge->name = 'LARGE';
+		$productLarge->listPrice = 261.90;
+		$productLarge->listPricePre = (strpos($productLarge->listPrice,'.') > -1) ? ''.$this->pricePreDec($productLarge->listPrice).'' : ''.$productLarge->listPrice.'';
+		$productLarge->listPricePost = (strpos($productLarge->listPrice,'.') > -1) ? ''.$this->pricePostDec($productLarge->listPrice).'' : ''; //currently set to show nothing in the <sup> tag. You can change it to '00' if you want.
+		$productLarge->price = 130.95;
+		$productLarge->pricePre = (strpos($productLarge->price,'.') > -1) ? ''.$this->pricePreDec($productLarge->price).'' : ''.$productLarge->price.'';
+		$productLarge->pricePost = (strpos($productLarge->price,'.') > -1) ? ''.$this->pricePostDec($productLarge->price).'' : ''; //currently set to show nothing in the <sup> tag. You can change it to '00' if you want.
+		$productLarge->shippingPrice = 0;
+		$productLarge->shippingText = 'Free Priority Shipping';
+		$productLarge->total = $productLarge->price + $productLarge->shippingPrice;
+		
+		$productMaximum = new \stdClass();
+		$productMaximum->id = 5;
+		$productMaximum->name = 'MAXIMUM';
+		$productMaximum->listPrice = 384;
+		$productMaximum->listPricePre = (strpos($productMaximum->listPrice,'.') > -1) ? ''.$this->pricePreDec($productMaximum->listPrice).'' : ''.$productMaximum->listPrice.'';
+		$productMaximum->listPricePost = (strpos($productMaximum->listPrice,'.') > -1) ? ''.$this->pricePostDec($productMaximum->listPrice).'' : ''; //currently set to show nothing in the <sup> tag. You can change it to '00' if you want.
+		$productMaximum->price = 192;
+		$productMaximum->pricePre = (strpos($productMaximum->price,'.') > -1) ? ''.$this->pricePreDec($productMaximum->price).'' : ''.$productMaximum->price.'';
+		$productMaximum->pricePost = (strpos($productMaximum->price,'.') > -1) ? ''.$this->pricePostDec($productMaximum->price).'' : ''; //currently set to show nothing in the <sup> tag. You can change it to '00' if you want.
+		$productMaximum->shippingPrice = 0;
+		$productMaximum->shippingText = 'Free Priority Shipping';
+		$productMaximum->total = $productMaximum->price + $productMaximum->shippingPrice;
+		
+		
 
         $currentItem = new \stdClass();
         $currentItem->product = new \stdClass();
-        $currentItem->product->id = 4;
+        $currentItem->product->id = 5;
 
         $form = $this->createFormBuilder()
             ->add('shippingAddress')
@@ -52,7 +119,7 @@ class DefaultController extends Controller
             ->getForm()
             ;
 
-        return array(
+        /*return array(
             'entity' => $entity,
             'form' => $form->createView(),
             'productTrial' => $productTrial,
@@ -66,10 +133,44 @@ class DefaultController extends Controller
             'currentItem' => $currentItem,
             'quickpay_agreementid' => '1778',
             'quickpay_merchantid' => '713',
+        );*/
+		return array(
+            'entity' => $entity,
+            'form' => $form->createView(),
+            'productSingle' => $productSingle,
+            'productBundleOffer' => $productBundleOffer,
+            'productStarter' => $productStarter,
+            'productLarge' => $productLarge,
+            'productMaximum' => $productMaximum,
+            'currentItem' => $currentItem,
+            'quickpay_agreementid' => '1778',
+            'quickpay_merchantid' => '713',
         );
     }
 
-    /**
+    public function pricePreDec($price)
+	{
+		$price = number_format((float)$price, 2, '.', '');
+		$brokenprice = array();
+		$brokenprice = explode('.',$price, 2);
+		//whether or not their is a decimal, we will always return the first element, we just check for decimal.
+		if(sizeof($brokenprice) == 2)
+			return ''.$brokenprice[0].'.';//returning as string as this will only be used for display
+		else
+			return ''.$brokenprice[0].'';//returning as string as this will only be used for display
+	}
+	public function pricePostDec($price)
+	{
+		$price = number_format((float)$price, 2, '.', '');
+		$brokenprice = array();
+		$brokenprice = explode('.',$price, 2);
+		if(sizeof($brokenprice) == 2)
+			return ''.$brokenprice[1].''; //returning as string as this will only be used for display
+		else
+			return '00'; //returning as string as this will only be used for display
+	}
+	
+	/**
      * @Route("/meh", name="avenue_checkout_ajax_replaceproduct")
      */
     public function mehAction()
